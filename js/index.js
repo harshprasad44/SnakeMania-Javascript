72f2cd9481eeab9d262dace1ac116ddfa319fef4
Harsh Prasad <explorerharsh@gmail.com>
2021-05-25 21:01:20 +0530

    requestAnimationFrame2

--- js/index.js
+++ js/index.js
@@ -4,13 +4,22 @@ const foodSound = new Audio("../music/food.mp3");
 const gameOverSound = new Audio("../music/gameover.mp3");
 const moveSound = new Audio("../music/move.mp3");
 const musicSound = new Audio("../music/music.mp3");
+let speed = 2;
+let lastPaintTime = 0;
 
 // Game functions
 
 function main(ctime) {
   window.requestAnimationFrame(main);
   // console.log(ctime);
+  if ((ctime - lastPaintTime) / 1000 < 1 / speed) {
+    return;
+  }
+  lastPaintTime = ctime;
+  gameEngine();
 }
+
+function gameEngine() {}
 
 // Main logic starts here
 window.requestAnimationFrame(main);
