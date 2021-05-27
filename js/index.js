11fdafa5ee0250fed00818eb26384bf3f67337ff
Harsh Prasad <explorerharsh@gmail.com>
2021-05-28 02:39:47 +0530

    Updating the snake arr and food

--- js/index.js
+++ js/index.js
@@ -1,10 +1,11 @@
 // Game constants and variables
-let direction = { x: 0, y: 0 };
+let inputDir = { x: 0, y: 0 };
 const foodSound = new Audio("../music/food.mp3");
 const gameOverSound = new Audio("../music/gameover.mp3");
 const moveSound = new Audio("../music/move.mp3");
 const musicSound = new Audio("../music/music.mp3");
 let speed = 2;
+let score = 0;
 let lastPaintTime = 0;
 let snakeArr = [{ x: 13, y: 15 }];
 food = { x: 6, y: 7 };
@@ -23,9 +24,17 @@ function main(ctime) {
 
 function gameEngine() {
   // Updating the snake array and food
+  if (isCollide(snakeArr)) {
+    gameOverSound.play();
+    musicSound.pause();
+    inputDir = { x: 0, y: 0 };
+    alert("Game Over. Press any key to play again.");
+    snakeArr = [{ x: 13, y: 15 }];
+    musicSound.play();
+    score = 0;
+  }
 
   // Display the snake and food
-
   // Display the snake
   board.innerHTML = "";
   snakeArr.forEach((e, index) => {
