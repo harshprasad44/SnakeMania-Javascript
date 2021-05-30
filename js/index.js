4a30626453308c69383d023699ac3e3ab422f3d8
Harsh Prasad <explorerharsh@gmail.com>
2021-05-30 10:21:37 +0530

    Collision and moving the snake

--- js/index.js
+++ js/index.js
@@ -22,8 +22,18 @@ function main(ctime) {
   gameEngine();
 }
 
-function isCollide(sarr) {
-  return false;
+function isCollide(snake) {
+  // if you bump into yourself
+  for (let i = 1; i < snakeArr.length; i++) {
+    if (snake[i].x === snake[0].x && snake[i].y === snake[0].y) {
+      return true;
+    }
+  }
+
+  // if you bump into the wall
+  if (snake[0].x >= 18 || (snake[0].x <= 0 && snake[0].y >= 18) || snake[0].y <= 0) {
+    return true;
+  }
 }
 
 function gameEngine() {
@@ -37,14 +47,23 @@ function gameEngine() {
     musicSound.play();
     score = 0;
   }
-  console.log(snakeArr);
+
   // If you have eaten the food, increament the score and regenrate the food
   if (snakeArr[0].y === food.y && snakeArr[0].x === food.y) {
+    foodSound.play();
     snakeArr.unshift({ x: snakeArr[0].x + inputDir.x, y: snakeArr[0].y + inputDir.y });
     let a = 2;
     let b = 16;
     food = { x: Math.round(a + (b - a) * Math.random()), y: Math.round(a + (b - a) * Math.random()) };
   }
+
+  // Moving the snake
+  for (let i = snakeArr.length - 2; i >= 0; i--) {
+    snakeArr[i + 1] = { ...snakeArr[i] };
+  }
+
+  snakeArr[0].x += inputDir.x;
+  snakeArr[0].y += inputDir.y;
 
   // Display the snake and food
   // Display the snake
